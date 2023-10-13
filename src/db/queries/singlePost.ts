e3bddb59b801cba23127ae60dcbd7dcd3718c25d
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-10-12 17:45:53 -0700

    Add single post

---
+++ src/db/queries/singlePost.ts
@@ -0,0 +1,30 @@
+import { db, eq } from "@/db";
+
+import { postTable } from "@/db/schema/post";
+import { userTable } from "@/db/schema/user";
+import { mediaTable } from "@/db/schema/media";
+
+export const query = db
+  .select({
+    id: postTable.id,
+    content: postTable.content,
+    createdAt: postTable.createdAt,
+    user: {
+      id: userTable.id,
+      username: userTable.username,
+      avatar: userTable.avatar,
+    },
+    media: {
+      id: mediaTable.id,
+      type: mediaTable.type,
+      url: mediaTable.url,
+      width: mediaTable.width,
+      height: mediaTable.height,
+    },
+  })
+  .from(postTable)
+  .innerJoin(userTable, eq(userTable.id, postTable.userId))
+  .leftJoin(mediaTable, eq(mediaTable.id, postTable.mediaId))
+  .prepare("select_posts_for_feed");
+
+export type Result = Awaited<ReturnType<typeof query.execute>>[0];
