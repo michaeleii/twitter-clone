c065eb57cb95ef4445022c68e1c0d9f103ca825b
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-11-11 14:10:40 -0800

    Update signout button styling

--- src/app/profile/signout-button.tsx
+++ src/app/profile/signout-button.tsx
@@ -1,5 +1,9 @@
 "use client";
 
 export default function SignoutButton({ signOut }: { signOut: () => void }) {
-  return <button onClick={signOut}>Sign Out</button>;
+  return (
+    <button className="border rounded-xl px-4 py-2 disabled" onClick={signOut}>
+      Sign Out
+    </button>
+  );
 }
