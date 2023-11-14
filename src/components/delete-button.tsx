70e74d234985a84fd8aaa54ce08b4138a709f67f
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-11-13 19:20:49 -0800

    Add disabled state to DeleteButton component

--- src/components/delete-button.tsx
+++ src/components/delete-button.tsx
@@ -7,7 +7,7 @@ export default function DeleteButton({ id }: { id: number }) {
   const [isPending, startTransition] = useTransition();
   return (
     <button
-      className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+      className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-900"
       disabled={isPending}
       onClick={async () => startTransition(async () => await deletePost(id))}
     >
